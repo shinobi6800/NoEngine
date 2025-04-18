4fc1077d4a52edc3739bd378e2ba37b18167a48f
Divine203 <divine00329@gmail.com>
2025-04-18 20:31:26 +0100

    textures

--- main.js
+++ main.js
@@ -12,9 +12,71 @@ const CH2 = CH / 2;
 const texture = new Image();
 texture.src = 'wall4.jpg';
 
+
 let angle = 0;
 let cameraZ = 1000;
 const fov = 500;
+
+const drawLine = (x1, y1, x2, y2) => {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = "white";
+    ctx.stroke();
+}
+
+function drawTxOnFace(tl, bl, tr, br) {
+    const steps = Math.ceil(Math.max(
+        Math.hypot(bl.x - tl.x, bl.y - tl.y),
+        Math.hypot(br.x - tr.x, br.y - tr.y)
+    ));
+
+
+    for (let i = 0; i < steps; i += 1) {
+        const t = i / steps;
+        const texY = Math.floor(t * texture.height);
+
+        // Interpolate points across the face
+        const leftX = tl.x + (bl.x - tl.x) * t;
+        const leftY = tl.y + (bl.y - tl.y) * t;
+
+        const rightX = tr.x + (br.x - tr.x) * t;
+        const rightY = tr.y + (br.y - tr.y) * t;
+
+        // Draw the texture strip at the interpolated position
+        drawTextureLine(leftX, leftY, rightX, rightY, texY);
+    }
+}
+
+function drawTextureLine(x1, y1, x2, y2, texY) {
+    // Width of the interpolated line
+    const width = Math.hypot(x2 - x1, y2 - y1);
+
+    // Angle of the line
+    const angle = Math.atan2(y2 - y1, x2 - x1);
+
+    ctx.save();
+
+    // Move to start point
+    ctx.translate(x1, y1);
+    ctx.rotate(angle);
+
+    // Draw 1px strip from the texture onto the line
+    ctx.drawImage(
+        texture,
+        0, 
+        texY, 
+        texture.width, 
+        1, // Source: 1 row from texture
+        0, 
+        0,
+        width, 
+        1           // Dest: stretch across the line
+    );
+
+    ctx.restore();
+}
+
 
 
 let cameraRotX = 0; // look up/down
@@ -74,148 +136,62 @@ class Vector {
 
 let cameraPos = new Vector(0, 0, 0);
 
-const drawLine = (x1, y1, x2, y2) => {
-    ctx.beginPath();
-    ctx.moveTo(x1, y1);
-    ctx.lineTo(x2, y2);
-    ctx.strokeStyle = "white";
-    ctx.stroke();
-}
-
-function drawScanline(y, xStart, xEnd) {
-    ctx.beginPath();
-    ctx.moveTo(xStart, y);
-    ctx.lineTo(xEnd, y);
-    ctx.strokeStyle = 'blue';
-    ctx.stroke();
-}
-
-const drawTriangleScanlines = (p1, p2, p3) => {
-    const points = [p1, p2, p3].sort((a, b) => a.y - b.y);
-    const [v0, v1, v2] = points;
-
-    const interp = (a, b, t) => a + (b - a) * t;
-
-    function edge(x0, y0, x1, y1) {
-        const points = [];
-        const dy = y1 - y0;
-        for (let y = Math.ceil(y0); y <= Math.floor(y1); y++) {
-            const t = (y - y0) / dy;
-            const x = interp(x0, x1, t);
-            points.push({ x, y });
-        }
-        return points;
-    }
-
-    // Build edge lists
-    const left = edge(v0.x, v0.y, v2.x, v2.y); // long edge
-    const rightTop = edge(v0.x, v0.y, v1.x, v1.y);
-    const rightBottom = edge(v1.x, v1.y, v2.x, v2.y);
-
-    const fullRight = rightTop.concat(rightBottom);
-
-    const ymin = Math.ceil(v0.y);
-    const ymax = Math.floor(v2.y);
-
-    for (let i = 0; i < ymax - ymin + 1; i++) {
-        const leftX = left[i].x;
-        const rightX = fullRight[i].x;
-        const y = left[i].y;
-
-        const xStart = Math.min(leftX, rightX);
-        const xEnd = Math.max(leftX, rightX);
-
-        drawScanline(y, xStart, xEnd);
-    }
-}
-
 const P = [];
 const center = new Vector(CW2, CH2, 0);
 
 
 class Cube {
-    constructor({ x, y, z, size }) {
-        this.size = size;
+    constructor({ x, y, z, w = 100, h = 100, d = 100 }) {
         this.x = x;
         this.y = y;
         this.z = z;
 
-        this.V = []; // vertices
-        this.T = [
-            [0, 1, 2], [1, 3, 2],
-            [5, 4, 7], [4, 6, 7],
-            [4, 0, 6], [0, 2, 6],
-            [1, 5, 3], [5, 7, 3],
-            [4, 5, 0], [5, 1, 0],
-            [2, 3, 6], [3, 7, 6]
-        ]; // triangles
+        this.w = w / 2; // half width
+        this.h = h / 2; // half height
+        this.d = d / 2; // half depth
+
+        this.V = [];
+        this.F = [
+            [0, 1, 3, 2], // front
+            [4, 5, 7, 6], // back
+            [0, 2, 6, 4], // left
+            [1, 5, 7, 3], // right
+            [0, 4, 5, 1], // top
+            [2, 3, 7, 6]  // bottom
+        ];
+
         this.setUp();
     }
 
     setUp() {
-        this.V[0] = new Vector(-this.size + this.x, -this.size + this.y, -this.size + this.z);
-        this.V[1] = new Vector(this.size + this.x, -this.size + this.y, -this.size + this.z);
-        this.V[2] = new Vector(-this.size + this.x, this.size + this.y, -this.size + this.z);
-        this.V[3] = new Vector(this.size + this.x, this.size + this.y, -this.size + this.z);
-        this.V[4] = new Vector(-this.size + this.x, -this.size + this.y, this.size + this.z);
-        this.V[5] = new Vector(this.size + this.x, -this.size + this.y, this.size + this.z);
-        this.V[6] = new Vector(-this.size + this.x, this.size + this.y, this.size + this.z);
-        this.V[7] = new Vector(this.size + this.x, this.size + this.y, this.size + this.z);
-    }
-}
+        const x = this.x;
+        const y = this.y;
+        const z = this.z;
 
-class Plane {
-    constructor({ isHor, x, y, z, size }) {
-        this.isHor = isHor;
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.size = size;
-        this.V = []; // vertices
-        this.T = [
-            [0, 1, 2], [1, 3, 2],
-        ]; // triangles
-        this.setUp();
-    }
+        const w = this.w;
+        const h = this.h;
+        const d = this.d;
 
-    setUp() {
-        if (this.isHor) {
-            this.V[0] = new Vector(-this.size + this.x, -this.size + this.y, -this.size + this.z);
-            this.V[1] = new Vector(this.size + this.x, -this.size + this.y, -this.size + this.z);
-            this.V[2] = new Vector(-this.size + this.x, -this.size + this.y, this.size + this.z);
-            this.V[3] = new Vector(this.size + this.x, -this.size + this.y, this.size + this.z);
-        } else {
-            this.V[0] = new Vector(-this.size + this.x, -this.size + this.y, -this.size + this.z);
-            this.V[1] = new Vector(this.size + this.x, -this.size + this.y, -this.size + this.z);
-            this.V[2] = new Vector(-this.size + this.x, this.size + this.y, -this.size + this.z);
-            this.V[3] = new Vector(this.size + this.x, this.size + this.y, -this.size + this.z);
-        }
+        this.V[0] = new Vector(-w + x, -h + y, -d + z); // top-left-front
+        this.V[1] = new Vector(w + x, -h + y, -d + z);  // top-right-front
+        this.V[2] = new Vector(-w + x, h + y, -d + z);  // bottom-left-front
+        this.V[3] = new Vector(w + x, h + y, -d + z);   // bottom-right-front
+        this.V[4] = new Vector(-w + x, -h + y, d + z);  // top-left-back
+        this.V[5] = new Vector(w + x, -h + y, d + z);   // top-right-back
+        this.V[6] = new Vector(-w + x, h + y, d + z);   // bottom-left-back
+        this.V[7] = new Vector(w + x, h + y, d + z);    // bottom-right-back
     }
 }
 
 
-const cube1 = new Cube({ x: 100, y: 100, z: 0, size: 50 });
-const cube2 = new Cube({ x: -200, y: 100, z: 0, size: 150 });
-const cube3 = new Cube({ x: 300, y: 200, z: 0, size: 100 });
-
-const plane1 = new Plane({ isHor: true, x: 20, y: 850, z: 0, size: 600 });
+const cube1 = new Cube({ x: 0, y: 0, z: 0, w: 200, h: 100, d: 300 });
+const cube2 = new Cube({ x: -200, y: 0, z: 0, w: 100, h: 600, d: 100 });
+const cube3 = new Cube({ x: 300, y: 200, z: 0 });
 
 const cubes = [cube1, cube2, cube3];
-const planes = [plane1];
 
 const init = () => {
 
-}
-
-function drawTriangle(p1, p2, p3, fillColor = 'black') {
-    ctx.beginPath();
-    ctx.moveTo(p1.x, p1.y);
-    ctx.lineTo(p2.x, p2.y);
-    ctx.lineTo(p3.x, p3.y);
-    ctx.closePath();
-
-    ctx.fillStyle = fillColor;
-    ctx.fill();
 }
 
 
@@ -246,20 +222,27 @@ const projectWorld = (obj, queue) => {
         projected.push(proj2D);
     }
 
-    for (let tri of obj.T) {
-        const p1 = projected[tri[0]];
-        const p2 = projected[tri[1]];
-        const p3 = projected[tri[2]];
+    for(let fac of obj.F) {
+        let i1 = fac[0];
+        let i2 = fac[1];
+        let i3 = fac[2];
+        let i4 = fac[3];
 
-        if (p1 && p2 && p3) {
-            const avgZ = (p1.z + p2.z + p3.z) / 3;
+        let p1 = projected[i1];
+        let p2 = projected[i2];
+        let p3 = projected[i3];
+        let p4 = projected[i4];
+
+        if(p1 && p2 && p3 && p4) {
+            const avgZ = (p1.z + p2.z + p3.z + p4.z) / 4;
 
             queue.push({
                 p1,
                 p2,
                 p3,
+                p4,
                 z: avgZ,
-                color: 'red' // or get it from the object
+                color: 'red'
             });
         }
     }
@@ -287,42 +270,24 @@ const engine = () => {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, cvs.width, cvs.height);
 
-    let planeQueue = [];
     let objQueue = [];
-
-    for (let p of planes) {
-        projectWorld(p, planeQueue);
-    }
 
     for (let c of cubes) {
         projectWorld(c, objQueue);
     }
 
-
     // Sort triangles back to front
     objQueue.sort((a, b) => b.z - a.z);
-    planeQueue.sort((a, b) => b.z - a.z);
 
-    // Draw sorted triangles
-    for (let tri of planeQueue) {
-        drawTriangle(tri.p1, tri.p2, tri.p3, tri.color);
-        drawLine(tri.p1.x, tri.p1.y, tri.p2.x, tri.p2.y);
-        drawLine(tri.p2.x, tri.p2.y, tri.p3.x, tri.p3.y);
-        drawLine(tri.p3.x, tri.p3.y, tri.p1.x, tri.p1.y);
+    // Draw sorted faces
+    for (let f of objQueue) {
+        drawLine(f.p1.x, f.p1.y, f.p2.x, f.p2.y);
+        drawLine(f.p2.x, f.p2.y, f.p3.x, f.p3.y);
+        drawLine(f.p3.x, f.p3.y, f.p4.x, f.p4.y);
+        drawLine(f.p4.x, f.p4.y, f.p1.x, f.p1.y);
+
+        drawTxOnFace(f.p1, f.p4, f.p2, f.p3);
     }
-
-    // Draw sorted triangles
-    for (let tri of objQueue) {
-        // drawTriangle(tri.p1, tri.p2, tri.p3, tri.color);
-        drawLine(tri.p1.x, tri.p1.y, tri.p2.x, tri.p2.y);
-        drawLine(tri.p2.x, tri.p2.y, tri.p3.x, tri.p3.y);
-        drawLine(tri.p3.x, tri.p3.y, tri.p1.x, tri.p1.y);
-
-        drawTriangleScanlines(tri.p1, tri.p2, tri.p3);
-
-    }
-
-
 
     requestAnimationFrame(engine);
 }
